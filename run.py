d418114d1d4c6ef11bc7b90af5e67a6b005dbc61
SoroushGReza <mycodeschool22@outlook.com>
2023-04-20 17:00:45 +0000

    Parse crypto price response, Return crypto price in USD, Create get_rate function

--- run.py
+++ run.py
@@ -9,4 +9,14 @@ def get_price(crypto):
                    f"ids={crypto}&vs_currencies=usd")
                    # Send request to API
                    response = requests.get(api_url)
-                   
+                   # Parse response as JSON
+                   data = response.json()
+                   # Return the USD price of the cryptocurrency
+                   return data[crypto]["usd"]
+                except Exception as e:
+                    print(f"Error getting {crypto} price: {e}")
+                    return None
+
+
+# Function to get xchange rate between two currencies
+def get_rate(base, target):
