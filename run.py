0f1a3c1f8e2d654fa887e9960153fac33c225c96
SoroushGReza <mycodeschool22@outlook.com>
2023-04-20 16:51:55 +0000

    Create get_price function, Build crypto price API URL, Request crypto price from API

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
 import requests
 from key import EXCHANGERATE_API_KEY
+
+
+def get_price(crypto):
+    try:
+        # Build API URL
+        api_url = (f"https://api.coingecko.com/api/v3/simple/price?"
+                   f"ids={crypto}&vs_currencies=usd")
+                   # Send request to API
+                   response = requests.get(api_url)
+                   
